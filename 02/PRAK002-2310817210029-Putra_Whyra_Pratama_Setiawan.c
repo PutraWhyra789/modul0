64d6843cf71bb280389ee1324374a9f8e8a6e735
PutraWhyra789 <putragaul789@gmail.com>
2023-09-18 10:43:07 +0700

    urut berdasar bahasa

--- 02/PRAK002-2310817210029-Putra_Whyra_Pratama_Setiawan.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    char nama[23] = "Putra Whyra Pratama S.";
-    printf("Selamat Pagi, %s\n", nama);
-    printf("Selamat Siang, %s\n", nama);
-    printf("Selamat Malam, %s\n", nama);
-}
